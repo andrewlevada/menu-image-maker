122b2c1a60c6222dbc2603ba3c08ce94bc66f6c3
AndrewLevada <levada.andrew@gmail.com>
2022-08-05 11:53:27 +0300

    :sparkles: implemented image creation logic

--- src/image-maker.ts
+++ src/image-maker.ts
@@ -2,7 +2,7 @@ import { generate } from "text-to-image";
 
 async function generateImage(text: string): Promise<Buffer> {
 	const dataUri = await generate(text.trim(), {
-		fontPath: "assets/Cousine-Regular.ttf",
+		fontPath: "build/Cousine-Regular.ttf",
 		lineHeight: 26,
 		margin: 40,
 		maxWidth: 800
